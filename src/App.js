d64f6e8648782117b0680a4bb02e0b9bdfa52ffb
gazdagergo <info@gazdagergo.com>
2018-01-09 14:29:47 +0100

    init with basic component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import MyComponent from './MyComponent';
 
 export default class App extends Component {
   render() {
     return (
-      <MyComponent />
+      <div>Hello world!</div>
     );
   }
 }
