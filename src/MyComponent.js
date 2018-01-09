6578f2970f6cf4f6f81ab94f1106be0039373bac
gazdagergo <info@gazdagergo.com>
2018-01-09 14:27:06 +0100

    init with basic component

---
+++ src/MyComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class MyComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+      </div>
+    );
+  }
+}
