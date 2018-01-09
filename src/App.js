c1a39fea8274b12fae6aad4dbfb31c812a37a5f8
gazdagergo <info@gazdagergo.com>
2018-01-09 15:03:13 +0100

    button component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import Button from './Button';
 
-export default class App extends Component {
+class App extends Component {
+
   render() {
     return (
-      <div>Hello world!</div>
+      <div>
+        <div>Hello world!</div>
+        <Button />
+      </div>
     );
   }
 }
+
+export default App;
---
+++ src/Button.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+
+  render() {
+    return (
+      <button>Mán gomb?</button>
+    );
+  }
+}
+
+export default Button;
